03c8dd89a52278b388eb6d50519ec5023b650954
MFlisar <mflisar.development@gmail.com>
2018-01-07 18:33:38 +0100

    first commit

---
+++ src/main/java/com/michaelflisar/feedbackmanager/FeedbackBuilder.java
@@ -0,0 +1,110 @@
+package com.michaelflisar.feedbackmanager;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.text.Html;
+
+import com.michaelflisar.cachefileprovider.CachedFileProvider;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FeedbackBuilder {
+
+    private List<String> mReceivers = null;
+    private String mSubject = null;
+    private String mText = null;
+    private boolean mTextIsHtml = false;
+    private List<Object> mAttachments = null;
+
+    public FeedbackBuilder() {
+    }
+
+    public FeedbackBuilder addReceiver(String mail) {
+        if (mReceivers == null) {
+            mReceivers = new ArrayList<>();
+        }
+        mReceivers.add(mail);
+        return this;
+    }
+
+    public FeedbackBuilder withSubject(String subject) {
+        mSubject = subject;
+        return this;
+    }
+
+    public FeedbackBuilder withText(String text) {
+        mText = text;
+        mTextIsHtml = false;
+        return this;
+    }
+
+    public FeedbackBuilder withText(String text, boolean textIsHtml) {
+        mText = text;
+        mTextIsHtml = textIsHtml;
+        return this;
+    }
+
+    public FeedbackBuilder addFile(File file) {
+        if (mAttachments == null) {
+            mAttachments = new ArrayList<>();
+        }
+        mAttachments.add(Uri.fromFile(file));
+        return this;
+    }
+
+    public FeedbackBuilder addFile(Uri uri) {
+        if (mAttachments == null) {
+            mAttachments = new ArrayList<>();
+        }
+        mAttachments.add(uri);
+        return this;
+    }
+
+    public Intent buildIntent(Context context, String chooserTitle) {
+
+        if (mReceivers == null) {
+            throw new RuntimeException("FeedbackBuilder is missing a receiver!");
+        }
+
+        if (mText == null && mAttachments == null && mSubject == null) {
+            throw new RuntimeException("FeedbackBuilder - seems like you are trying to send an empty feedback, add a subject, attachment or some text!");
+        }
+
+        boolean single = mAttachments == null || mAttachments.size() == 1;
+
+        Intent intent = new Intent(single ? Intent.ACTION_SEND : Intent.ACTION_SEND_MULTIPLE);
+        intent.setType(single ? "message/rfc822" : "text/plain");
+        intent.putExtra(Intent.EXTRA_EMAIL, mReceivers.toArray(new String[mReceivers.size()]));
+        intent.putExtra(Intent.EXTRA_SUBJECT, mSubject);
+        if (mText != null) {
+            intent.putExtra(Intent.EXTRA_TEXT, mTextIsHtml ? Html.fromHtml(mText) : mText);
+        }
+
+        if (mAttachments != null) {
+            String cacheFileName = null;
+            if (mAttachments.size() == 1) {
+                cacheFileName = FeedbackUtil.copyFileToCache(context, mAttachments.get(0));
+                intent.putExtra(Intent.EXTRA_STREAM, CachedFileProvider.getCacheFileUri(context, cacheFileName));
+            } else {
+                ArrayList<Uri> uris = new ArrayList<>();
+                for (Object attachment : mAttachments) {
+                    cacheFileName = FeedbackUtil.copyFileToCache(context, attachment);
+                    uris.add(CachedFileProvider.getCacheFileUri(context, cacheFileName));
+                }
+                intent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, uris);
+            }
+        }
+
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        return Intent.createChooser(intent, chooserTitle);
+    }
+
+    public void startIntent(Context context, String chooserTitle) {
+        Intent intent = buildIntent(context, chooserTitle);
+        context.startActivity(intent);
+    }
+}
---
+++ src/main/java/com/michaelflisar/feedbackmanager/FeedbackUtil.java
@@ -0,0 +1,57 @@
+package com.michaelflisar.feedbackmanager;
+
+
+import android.content.Context;
+import android.net.Uri;
+
+import com.michaelflisar.cachefileprovider.CacheFileProviderUtil;
+
+import java.io.File;
+import java.util.UUID;
+
+public class FeedbackUtil {
+
+    static String copyFileToCache(Context context, Object file) {
+        if (file instanceof File) {
+            return copyFileToCache(context, (File) file);
+        } else if (file instanceof Uri) {
+            return copyFileToCache(context, (Uri) file);
+        } else {
+            throw new RuntimeException("Unknown file object!");
+        }
+    }
+
+    static String copyFileToCache(Context context, File file) {
+        // 1) find out a free cache file name
+        String cacheFileName = createCacheFileName(file);
+
+        // 2) copy input file to cache file
+        File cacheFile = CacheFileProviderUtil.copyFile(context, file, cacheFileName);
+
+        // 3) return cache file name
+        return cacheFileName;
+    }
+
+    static String copyFileToCache(Context context, Uri fileUri) {
+        // 1) find out a free cache file name
+        String cacheFileName = createCacheFileName(new File(fileUri.getPath()));
+
+        // 2) copy input file to cache file
+        Uri cacheFile = CacheFileProviderUtil.copyFile(context, fileUri, cacheFileName);
+
+        // 3) return cache file name
+        return cacheFileName;
+    }
+
+    static String createCacheFileName(File file) {
+        // find out a free cache file name - simply use filename + _ + UUID
+        String cacheFileName = file.getName() + "_" + UUID.randomUUID().toString();
+        String extension = "";
+        int i = file.getAbsolutePath().lastIndexOf('.');
+        if (i > 0) {
+            extension = file.getAbsolutePath().substring(i + 1);
+            cacheFileName += "." + extension;
+        }
+        return cacheFileName;
+    }
+}
