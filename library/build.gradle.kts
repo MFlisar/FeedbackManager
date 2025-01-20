0584d96f4d058e9707f7da93491e5e0f87410f65
Michael Flisar <Michael.Flisar@knapp.com>
2025-01-20 14:32:26 +0100

    new documentation

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -13,18 +13,36 @@ plugins {
 // Informations
 // -------------------
 
+val description = "This is a very small library that allows you to send feedback from an app without internet permission via email, either directly or via an unintrusive notification."
+
 // Module
 val artifactId = "library"
 val androidNamespace = "com.michaelflisar.feedbackmanager"
 
 // Library
 val libraryName = "FeedbackManager"
-val libraryDescription = "This is a very small library that allows you to send feedback from an app without internet permission via email, either directly or via an unintrusive notification."
+val libraryDescription = "FeedbackManager - $artifactId module - $description"
 val groupID = "io.github.mflisar.feedbackmanager"
 val release = 2017
 val github = "https://github.com/MFlisar/FeedbackManager"
 val license = "Apache License 2.0"
 val licenseUrl = "$github/blob/main/LICENSE"
+
+// -------------------
+// Variables for Documentation Generator
+// -------------------
+
+// # DEP + GROUP are optional arrays!
+
+// OPTIONAL = "false"               // defines if this module is optional or not
+// GROUP_ID = "core"                // defines the "grouping" in the documentation this module belongs to
+// DEP = "deps.cachefileprovider|CacheFileProvider|https://github.com/MFlisar/CacheFileProvider"
+// PLATFORM_INFO = ""               // defines a comment that will be shown in the documentation for this modules platform support
+
+// GLOBAL DATA
+// BRANCH = "master"        // defines the branch on github (master/main)
+// GROUP = "core|Core|core"
+// #GROUP = "modules|Modules|dialog modules"
 
 // -------------------
 // Setup
