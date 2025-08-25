b5461a0c636af5ad2496800785070b5c46ebff14
MFlisar <mflisar.development@gmail.com>
2025-08-25 12:01:37 +0200

    deps + kmp-library

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -26,36 +26,7 @@ val androidNamespace = "com.michaelflisar.feedbackmanager"
 
 kotlin {
     compilerOptions {
-        jvmTarget.set(JvmTarget.JVM_17)
-    }
-}
-
-android {
-
-    namespace = androidNamespace
-
-    compileSdk = app.versions.compileSdk.get().toInt()
-
-    defaultConfig {
-        minSdk = app.versions.minSdk.get().toInt()
-        vectorDrawables.useSupportLibrary = true
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
-            consumerProguardFiles("proguard-rules.pro")
-        }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+        jvmTarget.set(JvmTarget.fromTarget(buildFilePlugin.javaVersion()))
     }
 }
 
